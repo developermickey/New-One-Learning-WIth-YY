55dc556ec4046030b5546329853254cd0e6f4fc3
developermickey <mukesh.quazma@gmail.com>
2024-01-08 17:29:06 +0530

    header folder created

---
+++ artistic/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div className="w-full h-[130px] bg-zinc-400 text-white">
+      <div className="topbar w-full ">
+        <p>
+          We ship nationwide. 30-day return policy. Free standard shipping on
+          orders over $75.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
